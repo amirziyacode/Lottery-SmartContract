9a03d46f7e26fea2de7e820e8c757b153f537471
amirziyacode <amzdidi@gmail.com>
2026-02-24 09:44:50 +0330

    use event to enterRaffle and set a duration for pickWinner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 
 /**
  * @title Raffle
- * @author 0xD4rk3n
+ * @author Amirziya
  * @notice This is a simple raffle contract that allows users to enter a raffle by paying an entrance fee. The contract owner can pick a winner from the list of entrants.
  * @dev implements chailink VRF to pick a random winner from the list of entrants.
  */
@@ -17,24 +17,42 @@ contract Raffle {
     error Raffle_SendMoreEther();
 
     uint256 public immutable I_ENTRANCE_FEE;
+    address payable[] public sPlayers;
 
-    constructor(uint256 entranceFee) {
+    /** @dev duration of the lottery in seconde */
+    uint256 private immutable I_INTERVAL;
+
+    uint256 private lastTimeStamp;
+
+    /**  
+     * Events
+    */
+    event RaffleEnteranceFee(address indexed  player);
+
+    constructor(uint256 entranceFee,uint256 interval) {
         I_ENTRANCE_FEE = entranceFee;
+        I_INTERVAL = interval;
+        lastTimeStamp = block.timestamp;    
     }
 
-    function enterRaffle() public payable {  
+    function enterRaffle() external payable {  
         if(msg.value < I_ENTRANCE_FEE){
             revert Raffle_SendMoreEther();
         }
+        sPlayers.push(payable(msg.sender));
+        emit RaffleEnteranceFee(msg.sender);
      }
 
-    function pickWinner() public {   }
-
+    function pickWinner() external view {  
+        if(block.timestamp - lastTimeStamp < I_INTERVAL){
+            revert();
+        }
+     }
     /**
-     * Getter and Setter functions
+     *  Getter and Setter functions
      */
 
-    function getEntercFee() public view returns (uint256) {
+    function getEntercFee() external view returns (uint256) {
         return I_ENTRANCE_FEE;
     }
 }
